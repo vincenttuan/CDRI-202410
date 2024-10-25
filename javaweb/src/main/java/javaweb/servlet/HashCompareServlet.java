f3ac4ff4e45846ec39da1374148e6b4ba722d3df
NTPU <NTPU@503-tr>
2024-10-25 15:59:07 +0800

    web

--- javaweb/src/main/java/javaweb/servlet/HashCompareServlet.java
+++ javaweb/src/main/java/javaweb/servlet/HashCompareServlet.java
@@ -1,7 +1,15 @@
 package javaweb.servlet;
 
+import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 /**
  GET /hash/compare 得到 hash_compare.jsp
@@ -20,5 +28,20 @@ import jakarta.servlet.http.HttpServlet;
  * */
 @WebServlet("/hash/compare")
 public class HashCompareServlet extends HttpServlet {
+	private static Map<String, String> users = new ConcurrentHashMap<>();
+	static {
+		users.put("john", "");
+		users.put("mary", "");
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+	}
 	
 }
