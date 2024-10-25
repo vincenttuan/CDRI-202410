f10152fa573e11066e4a65d3b663b64ae3dc4b2e
NTPU <NTPU@503-tr>
2024-10-25 14:36:14 +0800

    web

---
+++ javaweb/src/main/java/javaweb/servlet/HashServlet.java
@@ -0,0 +1,44 @@
+package javaweb.servlet;
+
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.util.Base64;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * 網址: /hash?password=1234
+ * 得到: 
+ * 		1.hashPassword(不含鹽)
+ * 		2.salt + hashPassword(password+salt)(含鹽)
+ * */
+@WebServlet("/hash")
+public class HashServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String password = req.getParameter("password");
+		String hash = getHash(password);
+		resp.getWriter().println("hash:" + hash);
+	}
+	
+	// 產生雜湊
+	private String getHash(String password) {
+		try {
+			// 加密演算法: SHA-256
+			MessageDigest md = MessageDigest.getInstance("SHA-256");
+			// 進行加密
+			byte[] bytes = md.digest(password.getBytes());
+			// 將 byte[] 透過 Base64 編碼方便儲存
+			return Base64.getEncoder().encodeToString(bytes);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+}
