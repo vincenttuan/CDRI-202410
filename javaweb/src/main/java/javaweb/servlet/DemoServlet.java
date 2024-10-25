172786b24203a7259a703c231bbd4017b1ae8521
NTPU <NTPU@503-tr>
2024-10-25 10:21:57 +0800

    web

---
+++ javaweb/src/main/java/javaweb/servlet/DemoServlet.java
@@ -0,0 +1,31 @@
+package javaweb.servlet;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/demo/*")
+public class DemoServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String uri         = req.getRequestURI();
+		String url         = req.getRequestURL().toString();
+		int port           = req.getServerPort();
+		String contextPath = req.getContextPath();
+		String servletPath = req.getServletPath();
+		String pathInfo    = req.getPathInfo();
+		
+		resp.getWriter().println("uri: " + uri);
+		resp.getWriter().println("url: " + url);
+		resp.getWriter().println("port: " + port);
+		resp.getWriter().println("contextPath: " + contextPath);
+		resp.getWriter().println("servletPath: " + servletPath);
+		resp.getWriter().println("pathInfo: " + pathInfo);
+	}
+	
+}
