759ddcb7f7248f9f88962c8b57fa2e64ceaac2d2
NTPU <NTPU@503-tr>
2024-10-28 12:03:07 +0800

    web

--- javaweb/src/main/java/javaweb/repository/BaseDao.java
+++ javaweb/src/main/java/javaweb/repository/BaseDao.java
@@ -1,6 +1,8 @@
 package javaweb.repository;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class BaseDao {
 	protected static Connection conn;
@@ -9,5 +11,12 @@ public class BaseDao {
 		String username = "root";
 		String password = "abc123";
 		String dbUrl = "jdbc:mysql://localhost:3306web?serverTimezone=Asia/Taipei&characterEncoding=utf-8&useUnicode=true";
+		// 建立連線
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			conn = DriverManager.getConnection(dbUrl, username, password);
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
 	}
 }
