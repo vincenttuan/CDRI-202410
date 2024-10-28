f903fe4bebce1f8271da4cd9c8b852978596c985
NTPU <NTPU@503-tr>
2024-10-28 11:55:57 +0800

    web

---
+++ javaweb/src/main/java/javaweb/repository/UserDao.java
@@ -0,0 +1,27 @@
+package javaweb.repository;
+
+import java.util.List;
+
+import javaweb.model.entity.User;
+
+// User (DAO: Data Access Object)
+public interface UserDao {
+	// 多筆: 查詢所有使用者
+	List<User> findAllUsers();
+	
+	// 單筆: 根據 username 查詢該筆使用者
+	User getUser(String username);
+	
+	// 新增
+	void addUser(User user);
+	
+	// 修改 active 狀態
+	void updateUserActive(Boolean active);
+	
+	// 修改 role 狀態
+	void updateUserRole(String role);
+		
+	// 刪除: 根據 userId 來刪除
+	void deleteUser(Integer userId);
+		
+}
