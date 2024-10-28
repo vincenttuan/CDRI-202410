f43b73767e23ea683d146905f49420c93ab90531
NTPU <NTPU@503-tr>
2024-10-28 11:57:22 +0800

    web

---
+++ javaweb/src/main/java/javaweb/repository/UserDaoImpl.java
@@ -0,0 +1,45 @@
+package javaweb.repository;
+
+import java.util.List;
+
+import javaweb.model.entity.User;
+
+public class UserDaoImpl implements UserDao {
+
+	@Override
+	public List<User> findAllUsers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public User getUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUserActive(Boolean active) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateUserRole(String role) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteUser(Integer userId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
