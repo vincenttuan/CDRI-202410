9539305b18c5bf0490615a77d1c9ca1280d7f7de
NTPU <NTPU@503-tr>
2024-10-28 15:01:07 +0800

    web

---
+++ javaweb/src/main/java/javaweb/servlet/ProductServlet.java
@@ -0,0 +1,43 @@
+package javaweb.servlet;
+
+/**
+-- 商品 product
++------------+------------------+----------+----------------+
+| product_id | product_name     | price    | stock_quantity |
++------------+------------------+----------+----------------+
+| 1          | PC               | 30000.00 | 50             |
+| 2          | Mobile           | 15000.00 | 100            |
+| 3          | MusicBox         | 3000.00  | 200            |
+| 4          | Pad              | 20000.00 | 75             |
+| 5          | Watch            | 8000.00  | 150            |
++------------+------------------+----------+----------------+
+
+ -- 創建商品表
+create table if not exists product (
+	product_id int primary key auto_increment comment '商品Id',
+	product_name varchar(50) not null unique comment '商品名稱',
+	price decimal(10, 2) not null comment '商品價格',
+	stock_quantity int not null default 0 comment '商品庫存'
+); 
+
+
+ MVC + 自訂框架
+  
+  request   +----------------+             +----------------+          +------------+
+ ---------> | ProductServlet | ----------> | ProductService | -------> | ProductDao | ------->    MySQL
+            | (Controller)   | <---------- |                | <------- |            | <------- (web.product)
+  			+----------------+  ProductDto +----------------+  Product +------------+
+  			       |              (Dto)                       (Entity)
+  			       |
+  			       v
+  			+-------------+
+ <--------- | product.jsp |
+  response	|    (View)   |
+  			+-------------+                 
+ 
+ 查詢全部: GET  /product, /products
+ 
+*/ 
+public class ProductServlet {
+
+}
