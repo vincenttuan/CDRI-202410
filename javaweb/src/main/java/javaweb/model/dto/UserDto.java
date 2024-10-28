04e3ef3a72cb7250bc64ee2f0da84e11efb33987
NTPU <NTPU@503-tr>
2024-10-28 11:34:49 +0800

    web

---
+++ javaweb/src/main/java/javaweb/model/dto/UserDto.java
@@ -0,0 +1,14 @@
+package javaweb.model.dto;
+
+import lombok.Data;
+
+// UserDto 對應於 User entity
+// Dto 的屬性名稱建議可以與所對應的 Entity 物件不同
+@Data
+public class UserDto {
+	private Integer userId; // 使用者ID
+	private String username; // 使用者名稱
+	private String email; // 電子郵件
+	private Boolean active; // 帳號啟動
+	private String role; // 角色權限
+}
--- javaweb/src/main/java/javaweb/model/entity/User.java
+++ javaweb/src/main/java/javaweb/model/entity/User.java
@@ -1,5 +1,7 @@
 package javaweb.model.entity;
 
+import lombok.Data;
+
 // 代表 users 資料表的紀錄
 /**
  * -- 建立 users 資料表
@@ -13,6 +15,7 @@ package javaweb.model.entity;
    		role varchar(50) not null default 'ROLE_USER' comment '角色權限'
  	);
  */
+@Data
 public class User {
 	private Integer userId; // 使用者ID
 	private String username; // 使用者名稱
@@ -21,6 +24,5 @@ public class User {
 	private String email; // 電子郵件
 	private Boolean active; // 帳號啟動
 	private String role; // 角色權限
-	
-	
+		
 }
