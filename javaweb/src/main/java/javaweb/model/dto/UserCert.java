0173e59dd42ed138e2dc1a4ce66596248d9a7fc2
NTPU <NTPU@503-tr>
2024-10-30 11:44:50 +0800

    web

---
+++ javaweb/src/main/java/javaweb/model/dto/UserCert.java
@@ -0,0 +1,28 @@
+package javaweb.model.dto;
+
+// 使用者憑證
+// 登入成功之後會得到的憑證資料
+public class UserCert {
+	private Integer userId; // 使用者ID
+	private String username; // 使用者名稱
+	private String role; // 角色權限
+	
+	public UserCert(Integer userId, String username, String role) {
+		this.userId = userId;
+		this.username = username;
+		this.role = role;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public String getRole() {
+		return role;
+	}
+	
+}
