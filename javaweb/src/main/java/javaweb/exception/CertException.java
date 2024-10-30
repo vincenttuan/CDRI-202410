ae5e9185a89567fd04d4358074c6e46449791485
NTPU <NTPU@503-tr>
2024-10-30 12:04:06 +0800

    web

---
+++ javaweb/src/main/java/javaweb/exception/CertException.java
@@ -0,0 +1,7 @@
+package javaweb.exception;
+
+public class CertException extends Exception {
+	public CertException(String message) {
+		super(message);
+	}
+}
---
+++ javaweb/src/main/java/javaweb/service/CertService.java
@@ -0,0 +1,29 @@
+package javaweb.service;
+
+import javaweb.model.dto.UserCert;
+import javaweb.model.entity.User;
+import javaweb.repository.UserDao;
+import javaweb.repository.UserDaoImpl;
+import javaweb.utils.Hash;
+
+// 憑證服務
+public class CertService {
+	private UserDao userDao = new UserDaoImpl();
+	// 登入成功後可以取得憑證
+	public UserCert getCert(String username, String password) {
+		// 1.是否有此人
+		User user = userDao.getUser(username);
+		if(user == null) {
+			throw new UserNotFoundException("查無此人");
+		}
+		// 2.比對密碼
+		String passwordHash = Hash.getHash(password, user.getSalt());
+		if(!passwordHash.equals(user.getPasswordHash())) {
+			throw new PasswordInValidException("密碼錯誤");
+		}
+		// 3. 簽發憑證
+		UserCert userCert = new UserCert(user.getUserId(), user.getUsername(), user.getRole());
+		return userCert;
+	}
+	
+}
