1c1bf4221b138c70ec74f745a4c86dc687b77fc3
NTPU <NTPU@503-tr>
2024-10-30 12:07:28 +0800

    web

---
+++ javaweb/src/main/java/javaweb/exception/PasswordInvalidException.java
@@ -0,0 +1,13 @@
+package javaweb.exception;
+
+public class PasswordInvalidException extends CertException {
+	
+	public PasswordInvalidException() {
+		super("密碼無效");
+	}
+	
+	public PasswordInvalidException(String message) {
+		super(message);
+	}
+	
+}
