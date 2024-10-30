c71edcc67bb8b1bb46ae183c79ba4d7d611c43ae
NTPU <NTPU@503-tr>
2024-10-30 12:09:24 +0800

    web

--- javaweb/src/main/java/javaweb/service/CertService.java
+++ javaweb/src/main/java/javaweb/service/CertService.java
@@ -17,12 +17,14 @@ public class CertService {
 		// 1.是否有此人
 		User user = userDao.getUser(username);
 		if(user == null) {
-			throw new UserNotFoundException("查無此人");
+			//throw new UserNotFoundException("查無此人");
+			throw new UserNotFoundException();
 		}
 		// 2.比對密碼
 		String passwordHash = Hash.getHash(password, user.getSalt());
 		if(!passwordHash.equals(user.getPasswordHash())) {
-			throw new PasswordInvalidException("密碼錯誤");
+			//throw new PasswordInvalidException("密碼錯誤");
+			throw new PasswordInvalidException();
 		}
 		// 3. 簽發憑證
 		UserCert userCert = new UserCert(user.getUserId(), user.getUsername(), user.getRole());
