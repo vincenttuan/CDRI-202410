8f417a89794cf2dec94e0394a7d9b11ff4bde975
NTPU <NTPU@503-tr>
2024-11-01 16:29:27 +0800

    web

--- javaweb/src/main/java/javaweb/service/OrderService.java
+++ javaweb/src/main/java/javaweb/service/OrderService.java
@@ -15,9 +15,10 @@ public class OrderService {
 	
 	// 同時新增多筆訂單
 	// userId: 使用者 id
-	// productIds: 每一件商品的 id    [1, 2, 3, 4, 5]
+	// productIds: 每一件商品的 id   [1, 2, 3, 4, 5]
 	// unitPrices: 每一件商品的單價    [10, 20, 30, 40, 50]
 	// amounts:    每一件商品的購買數量 [5, 0, 3, 0, 2]
+    //                        i:  0  1  2  3  4
 	// 注意: productIds 的長度必須等於 amounts 的長度
 	public void batchAddOrders(Integer userId, String[] productIds, String[] unitPrices, String[] amounts) {
 		List<Order> orders = new ArrayList<>();
@@ -34,11 +35,10 @@ public class OrderService {
 			order.setQuantity(amount);
 			order.setUnitPrice(unitPrice); // ?
 			order.setSubtotal((int)(order.getQuantity() * order.getUnitPrice()));
-			order.setOrderStatus("Pending");
+			//order.setOrderStatus("Pending"); // 可不加因為資料表有預設值
 			
 			orders.add(order);
 		}
-		
 		
 		orderDao.batchAddOrders(orders);
 	}
