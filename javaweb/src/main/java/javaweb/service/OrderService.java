944effd199f677f554f816ddf97597461db5a1df
NTPU <NTPU@503-tr>
2024-11-01 16:13:41 +0800

    web

--- javaweb/src/main/java/javaweb/service/OrderService.java
+++ javaweb/src/main/java/javaweb/service/OrderService.java
@@ -1,5 +1,6 @@
 package javaweb.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javaweb.model.entity.Order;
@@ -12,10 +13,15 @@ public class OrderService {
 	
 	// 同時新增多筆訂單
 	// userId: 使用者 id
-	// productIds: 每一件商品的 id
-	// quantities: 每一件商品的購買數量
+	// productIds: 每一件商品的 id    [1, 2, 3, 4, 5]
+	// quantities: 每一件商品的購買數量 [5, 0, 3, 0, 2]
+	// 注意: productIds 的長度必須等於 quantities 的長度
 	public void batchAddOrders(Integer userId, String[] productIds, String[] quantities) {
+		List<Order> orders = new ArrayList<>();
 		
+		
+		
+		orderDao.batchAddOrders(orders);
 	}
 	
 }
