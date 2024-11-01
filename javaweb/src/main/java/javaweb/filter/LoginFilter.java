956866e3830e1e33e82ad8539dc28d12ed1e8f5f
NTPU <NTPU@503-tr>
2024-11-01 11:08:05 +0800

    web

--- javaweb/src/main/java/javaweb/filter/LoginFilter.java
+++ javaweb/src/main/java/javaweb/filter/LoginFilter.java
@@ -21,12 +21,15 @@ public class LoginFilter extends HttpFilter {
 		//response.setContentType("text/plain;charset=utf-8");
 		
 		System.out.println("攔截過濾 URL :" + request.getRequestURL());
+		System.out.println("攔截過濾 URI :" + request.getRequestURI());
 		//response.getWriter().println("攔截過濾 URL :" + request.getRequestURL());
 		
 		// 判斷是否有憑證
 		HttpSession session = request.getSession();
 		if(session.getAttribute("userCert") == null) {
 			//response.getWriter().println("請先登入");
+			// 記住此次請求的 URL
+			session.setAttribute("redirectURL", request.getRequestURL());
 			response.sendRedirect("/javaweb/login"); // 自動重導到登入頁面
 		} else {
 			chain.doFilter(request, response); // 放行
