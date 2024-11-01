1e42d23d25555b7b474dbf8c1f32353974914145
NTPU <NTPU@503-tr>
2024-11-01 15:49:18 +0800

    web

--- javaweb/src/main/java/javaweb/repository/OrderDao.java
+++ javaweb/src/main/java/javaweb/repository/OrderDao.java
@@ -6,10 +6,13 @@ import javaweb.model.entity.Order;
 
 public interface OrderDao {
 	
+	// 查找該使用者的所有訂單，根據訂單狀態進行篩選 
 	public List<Order> findAllOrders(Integer userId, String orderStatus);
 	
+	// 同時新增多筆訂單
 	public void batchAddOrders(List<Order> orders);
 	
+	// 同時更新多筆訂單的指定狀態
 	public void batchUpdateOrderStatus(List<Integer> orderIds, String orderStatus);
 	
 }
