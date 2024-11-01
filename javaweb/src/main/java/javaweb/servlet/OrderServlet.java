5da1537c554e1df479a59950f422142a8e4ccc10
NTPU <NTPU@503-tr>
2024-11-01 12:06:46 +0800

    web

--- javaweb/src/main/java/javaweb/servlet/OrderServlet.java
+++ javaweb/src/main/java/javaweb/servlet/OrderServlet.java
@@ -23,6 +23,21 @@ create table if not exists orders (
 	subtotal   decimal(10, 2) not null comment '小計',
 	order_status ENUM('Pending', 'Finished', 'Cancel') NOT NULL DEFAULT 'Pending' comment '訂單狀態'
 );
+
+insert into orders(user_id, order_date, product_id, quantity, unit_price, subtotal, order_status) 
+values(1, '2024-09-19', 1, 2, 30000.00, 60000.00, 'Finished');
+
+insert into orders(user_id, order_date, product_id, quantity, unit_price, subtotal, order_status) 
+values(1, '2024-09-20', 2, 5, 15000.00, 75000.00, 'Finished');
+
+insert into orders(user_id, order_date, product_id, quantity, unit_price, subtotal, order_status) 
+values(3, '2024-09-21', 3, 3, 3000.00, 9000.00, 'Pending');
+
+insert into orders(user_id, order_date, product_id, quantity, unit_price, subtotal, order_status) 
+values(4, '2024-09-22', 2, 1, 15000.00, 15000.00, 'Cancel');
+
+insert into orders(user_id, order_date, product_id, quantity, unit_price, subtotal, order_status) 
+values(5, '2024-09-23', 5, 4, 8000.00, 32000.00, 'Pending');
  * */
 
 import java.io.IOException;
