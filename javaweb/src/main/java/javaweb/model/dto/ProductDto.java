8780269f130980234c88dff6459458b79d9e1e94
NTPU <NTPU@503-tr>
2024-11-04 13:59:52 +0800

    web

--- javaweb/src/main/java/javaweb/model/dto/ProductDto.java
+++ javaweb/src/main/java/javaweb/model/dto/ProductDto.java
@@ -1,8 +1,12 @@
 package javaweb.model.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductDto {
 	private Integer productId;
 	private String productName;
---
+++ javaweb/src/main/java/javaweb/service/ProductService.java
@@ -0,0 +1,24 @@
+package javaweb.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javaweb.model.dto.ProductDto;
+import javaweb.model.entity.Product;
+import javaweb.repository.ProductDao;
+import javaweb.repository.ProductDaoImpl;
+
+public class ProductService {
+	private ProductDao productDao = new ProductDaoImpl();
+	
+	public List<ProductDto> findAllProducts() {
+		List<Product> products = productDao.findAllProducts();
+		List<ProductDto> productDtos = new ArrayList<>();
+		
+		products.stream().forEach((p) -> {
+			productDtos.add(new ProductDto(p.getProductId(), p.getProductName(), p.getPrice(), p.getStockQuantity()));
+		});
+		
+		return productDtos;
+	}
+}
