25c7818fb63a3703b4853bec8ba9517a669cb75e
NTPU <NTPU@503-tr>
2024-11-04 16:11:03 +0800

    web

--- javaweb/src/main/java/javaweb/repository/ProductDao.java
+++ javaweb/src/main/java/javaweb/repository/ProductDao.java
@@ -1,9 +1,12 @@
 package javaweb.repository;
 
 import java.util.List;
+import java.util.Map;
 
 import javaweb.model.entity.Product;
 
 public interface ProductDao {
 	List<Product> findAllProducts();
+	// Map<商品名稱(product_name), 銷售金額 (total_sales)>
+	List<Map<String, Double>> querySalesRanking(); // 銷售排行
 }
