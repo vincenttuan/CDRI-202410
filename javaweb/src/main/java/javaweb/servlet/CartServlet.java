3a9bc48d0642dab86c8ec6c08bb2875c854f2bc0
NTPU <NTPU@503-tr>
2024-11-04 10:30:34 +0800

    web

--- javaweb/src/main/java/javaweb/servlet/CartServlet.java
+++ javaweb/src/main/java/javaweb/servlet/CartServlet.java
@@ -1,12 +1,16 @@
 package javaweb.servlet;
 
 import java.io.IOException;
+import java.util.List;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import javaweb.model.dto.OrderDto;
+import javaweb.model.dto.UserCert;
 import javaweb.service.OrderService;
 
 @WebServlet("/cart")
@@ -17,7 +21,12 @@ public class CartServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		
+		HttpSession session = req.getSession();
+		UserCert userCert = (UserCert)session.getAttribute("userCert");
 		
+		List<OrderDto> orderDtos = orderService.findAllOrders(userCert.getUserId(), "Pending");
+		
+		resp.getWriter().print(orderDtos);
 		
 	}
 	
