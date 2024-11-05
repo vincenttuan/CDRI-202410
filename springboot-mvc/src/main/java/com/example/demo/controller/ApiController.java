61e18249634abf7dcdc713a80d7f0bb958c9d7b4
NTPU <NTPU@503-tr>
2024-11-05 12:02:16 +0800

    web

--- springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
+++ springboot-mvc/src/main/java/com/example/demo/controller/ApiController.java
@@ -30,11 +30,17 @@ public class ApiController {
 	 * 路徑: /greet?name=John&age=18
 	 * 路徑: /greet?name=Mary
 	 * 網址: http://localhost:8080/api/greet?name=John&age=18
-	 * 結果: John, 18 (成年)
+	 * 結果: Hi John, 18 (成年)
 	 * 網址: http://localhost:8080/api/greet?name=Mary
-	 * 結果: Mary, 0 (未成年)
+	 * 結果: Hi Mary, 0 (未成年)
 	 * 限制: name 參數一定要加, age 參數可不加(若沒有加 age 參數則會給初始值 0)
 	 * */
-	
+	@GetMapping("/greet")
+	public String greet(@RequestParam(value = "name", required = true) String name,
+						@RequestParam(value = "age", required = false, defaultValue = "0") Integer age) {
+		
+		return String.format("Hi %s, %d (%s)", name, age, age >= 18?"成年":"未成年");
+		
+	}
 	
 }
