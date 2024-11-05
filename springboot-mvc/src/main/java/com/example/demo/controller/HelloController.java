420c8bddd4aeb5937ac3918c1aee1d6299734dad
NTPU <NTPU@503-tr>
2024-11-05 11:10:18 +0800

    web

---
+++ springboot-mvc/src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hi")
+	@ResponseBody // 直接回傳值
+	public String sayHi() {
+		return "Hi";
+	}
+	
+	@GetMapping("/yes")
+	@ResponseBody // 直接回傳值
+	public String sayYes() {
+		return "Yes";
+	} 
+	
+}
