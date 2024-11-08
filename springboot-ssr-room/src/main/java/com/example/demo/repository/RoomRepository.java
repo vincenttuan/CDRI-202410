812f8901851e272a0545ee1dd62ddc3f7a75e112
NTPU <NTPU@503-tr>
2024-11-08 16:31:14 +0800

    web

--- springboot-ssr-room/src/main/java/com/example/demo/repository/RoomRepository.java
+++ springboot-ssr-room/src/main/java/com/example/demo/repository/RoomRepository.java
@@ -16,11 +16,11 @@ public interface RoomRepository extends JpaRepository<Room, Integer> { // Room: 
 	// 1. 查詢 roomSize 大於指定值的房間(自動生成 SQL)
 	List<Room> findByRoomSizeGreaterThen(Integer size);
 	
-	// 2. 查詢 roomSize 大於指定值的房間(JPQL)
+	// 2. 查詢 roomSize 大於指定值的房間(JPQL 以 entity 來操作)
 	@Query("select r from Room r where r.roomSize > :size")
 	List<Room> findRoomsBySizeGreaterThen1(Integer size);
 	
-	// 3. 查詢 roomSize 大於指定值的房間(SQL)
+	// 3. 查詢 roomSize 大於指定值的房間(SQL 以 table 來操作)
 	@Query(value = "select room_id, room_name, room_size from room where room_size > :size", nativeQuery = true)
 	List<Room> findRoomsBySizeGreaterThen2(Integer size);
 }
