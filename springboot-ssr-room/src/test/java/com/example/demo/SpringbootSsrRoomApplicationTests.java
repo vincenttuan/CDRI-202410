f70618ad302acefc005668444282fa0b87f5f4c6
NTPU <NTPU@503-tr>
2024-11-08 13:49:10 +0800

    web

--- springboot-ssr-room/src/test/java/com/example/demo/SpringbootSsrRoomApplicationTests.java
+++ springboot-ssr-room/src/test/java/com/example/demo/SpringbootSsrRoomApplicationTests.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import com.example.demo.mapper.RoomMapper;
+import com.example.demo.model.dto.RoomDto;
+import com.example.demo.model.entity.Room;
 
 @SpringBootTest
 class SpringbootSsrRoomApplicationTests {
@@ -14,6 +16,17 @@ class SpringbootSsrRoomApplicationTests {
 	
 	@Test
 	void testRoomMapper() {
+		// Entity
+		Room room = new Room(101, "101(S)", 4);
+		
+		// toDto 將 Entity 轉 DTO
+		RoomDto roomDto = roomMapper.toDto(room);
+		System.out.println("測試 toDto: " + roomDto);
+		
+		// toEntity 將 DTO 轉 Entity
+		room = roomMapper.toEntity(roomDto);
+		System.out.println("測試 toEntity: " + room);
+		
 		
 	}
 
