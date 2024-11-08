3e17ffdedbe9e05e699283af856b82c79c148dd7
NTPU <NTPU@503-tr>
2024-11-08 13:45:17 +0800

    web

--- springboot-ssr-room/src/test/java/com/example/demo/SpringbootSsrRoomApplicationTests.java
+++ springboot-ssr-room/src/test/java/com/example/demo/SpringbootSsrRoomApplicationTests.java
@@ -1,13 +1,19 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.mapper.RoomMapper;
 
 @SpringBootTest
 class SpringbootSsrRoomApplicationTests {
 	
+	@Autowired
+	private RoomMapper roomMapper;
+	
 	@Test
-	void contextLoads() {
+	void testRoomMapper() {
 		
 	}
 
