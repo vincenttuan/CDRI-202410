d9ea3dfc3ab962de5a53354e348b93f4951caa10
NTPU <NTPU@503-tr>
2024-11-11 14:19:44 +0800

    web

--- springboot-ssr-room/src/main/java/com/example/demo/controller/RoomController.java
+++ springboot-ssr-room/src/main/java/com/example/demo/controller/RoomController.java
@@ -9,6 +9,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -22,12 +23,11 @@ import jakarta.validation.Valid;
 /**
  * Method URI            功能
  * --------------------------------------------------------------------
- * GET    /rooms         查詢所有會議室(多筆)
- * GET    /room/{roomId} 查詢指定會議室(單筆)
- * POST   /room          新增會議室
- * PUT    /room/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
- * PATCH  /room/{roomId} 部分欄位修改會議室(只修改  roomName 或 roomSize etc…)
- * DETETE /room/{roomId} 刪除會議室
+ * GET    /rooms                查詢所有會議室(多筆)
+ * GET    /room/{roomId}        查詢指定會議室(單筆)
+ * POST   /room                 新增會議室
+ * POST   /room/update/{roomId} 完整修改會議室(同時修改 roomName 與 roomSize)
+ * GET    /room/delete/{roomId} 刪除會議室
  * --------------------------------------------------------------------
  * */
 
@@ -55,6 +55,12 @@ public class RoomController {
 			return "room"; // 會自動將錯誤訊息傳給 jsp
 		}
 		roomService.addRoom(roomDto);
+		return "redirect:/rooms"; // 重導到 /rooms 頁面
+	}
+	
+	@GetMapping("/delete/{roomId}")
+	public String deleteRoom(@PathVariable("roomId") Integer roomId) {
+		roomService.deleteRoom(roomId);
 		return "redirect:/rooms"; // 重導到 /rooms 頁面
 	}
 	
