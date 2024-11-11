8566d2ea6db95ec37cbb45d002ccf732c87a0014
NTPU <NTPU@503-tr>
2024-11-11 15:49:34 +0800

    web

--- springboot-ssr-room/src/main/java/com/example/demo/controller/LoginController.java
+++ springboot-ssr-room/src/main/java/com/example/demo/controller/LoginController.java
@@ -1,14 +1,19 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.demo.service.UserService;
+
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	@Autowired
+	private UserService userService;
 	
 	@GetMapping
 	public String loginPage() {
@@ -17,7 +22,7 @@ public class LoginController {
 	
 	@PostMapping
 	public String checkLogin(@RequestParam String username, @RequestParam String password) {
-		
+		// 透過 userService 來確認登入
 		
 		return "redirect:/rooms"; // 登入成功後到首頁
 	}
