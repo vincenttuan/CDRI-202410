b50ebb4f13873001816835257b459c9db46f3ba5
NTPU <NTPU@503-tr>
2024-11-11 15:29:40 +0800

    web

---
+++ springboot-ssr-room/src/main/java/com/example/demo/controller/LoginController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/login")
+public class LoginController {
+	
+	@GetMapping
+	public String loginPage() {
+		return "login";
+	}
+	
+	@PostMapping
+	public String checkLogin(@RequestParam String username, @RequestParam String password) {
+		return "login";
+	}
+	
+}
