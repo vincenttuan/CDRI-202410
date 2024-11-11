41ff95e5b83defab40c7e39e63ee8b98a0fc204c
NTPU <NTPU@503-tr>
2024-11-11 12:23:03 +0800

    web

--- springboot-ssr-room/src/main/java/com/example/demo/model/dto/RoomDto.java
+++ springboot-ssr-room/src/main/java/com/example/demo/model/dto/RoomDto.java
@@ -1,5 +1,10 @@
 package com.example.demo.model.dto;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.Range;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,10 +14,14 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RoomDto {
 	
+	@NotNull(message = "房號不可為空")
 	private Integer roomId;
 	
+	@NotNull(message = "房名不可為空")
 	private String roomName;
 	
+	@NotNull(message = "人數不可為空")
+	@Range(min = 1, max = 500, message = "房間人數必須介於 {min} ~ {max} 人之間")
 	private Integer roomSize;
 	
 }
