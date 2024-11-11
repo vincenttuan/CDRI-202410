60256a846d733e75a2d0f83a446110dd7cbcbb9a
NTPU <NTPU@503-tr>
2024-11-11 09:57:58 +0800

    web

---
+++ springboot-ssr-room/src/main/java/com/example/demo/service/impl/RoomServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.demo.service.impl;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.model.dto.RoomDto;
+import com.example.demo.service.RoomService;
+
+@Service
+public class RoomServiceImpl implements RoomService {
+
+	@Override
+	public List<RoomDto> getAllRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RoomDto getRoomById(Integer roomId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void addRoom(RoomDto roomDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void addRoom(Integer roomId, String roomName, Integer roomSize) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoom(Integer roomId, RoomDto roomDto) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void updateRoom(Integer roomId, String roomName, Integer roomSize) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void deleteRoom(Integer roomId) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
