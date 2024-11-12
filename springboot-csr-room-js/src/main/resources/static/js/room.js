45edd42017abb95b09a42a9bff4a6ae309f01346
NTPU <NTPU@503-tr>
2024-11-12 11:13:58 +0800

    web

--- springboot-csr-room-js/src/main/resources/static/js/room.js
+++ springboot-csr-room-js/src/main/resources/static/js/room.js
@@ -7,9 +7,21 @@ const fetchRooms = async () => {
 		const response = await fetch('http://localhost:8081/rest/room');
 		const apiResponse = await response.json();
 		console.log(apiResponse);
+		displayRooms(apiResponse.data);
 	} catch(e) {
 		console.error('遠端資料存取錯誤:', e);	
 	}
+};
+
+// 顯示房間列表
+const displayRooms = (rooms) => {
+	
+	rooms.forEach(room => {
+		const listItem = document.createElement('li'); // 建立 <li> 標籤元素
+		listItem.textContent = `房號: ${room.roomId} 房名: ${room.roomName} - 人數: ${room.roomSize}`;
+		// 將 listItem 加入到 roomList 中
+		roomList.appendChild(listItem);
+	});
 };
 
 
