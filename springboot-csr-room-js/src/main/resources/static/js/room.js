fe9e36a43206e0ede685711ef499715fb47e7733
NTPU <NTPU@503-tr>
2024-11-12 12:25:37 +0800

    web

--- springboot-csr-room-js/src/main/resources/static/js/room.js
+++ springboot-csr-room-js/src/main/resources/static/js/room.js
@@ -1,5 +1,10 @@
-// 獲取 DOM 元素
+// 獲取 DOM 元素 - 查詢房間
 const roomList = document.getElementById('roomList');
+// 獲取 DOM 元素 - 新增房間
+const roomIdInput = document.getElementById('roomId');
+const roomNameInput = document.getElementById('roomName');
+const roomSizeInput = document.getElementById('roomSize');
+const addResultText = document.getElementById('addResult');
 
 // 透過 fetch 經由 http://localhost:8081/rest/room 取得遠端資料
 const fetchRooms = async () => {
@@ -23,6 +28,69 @@ const displayRooms = (rooms) => {
 	});
 };
 
+// 新增房間
+const addRoom = async () => {
+	const roomId = roomIdInput.value;
+	const roomName = roomNameInput.value;
+	const roomSize = roomSizeInput.value;
+	
+	// 檢查資料
+	if(!roomId || !roomName || !roomSize) {
+		addResultText.textContent = '請輸入 id, name 與 size';
+		return;
+	}
+	
+	// 遠端新增程序
+	try {
+		// 將資料轉 json 物件
+		const roomDto = {
+			roomId: roomId,
+			roomName: roomName,
+			roomSize: roomSize
+		};
+		
+		const response = await fetch('http://localhost:8081/rest/room', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json'
+			},
+			body: JSON.stringify(roomDto) // 轉 json string 後送出
+		});
+		
+		const apiResponse = await response.json();
+		addResultText.textContent = apiResponse.message;
+		
+		if(response.ok) {
+			// 重新查詢房間列表資料
+			fetchRooms(); 
+			// 清空新增房間的表單欄位
+			roomIdInput.value = '';
+			roomNameInput.value = '';
+			roomSizeInput.value = '';	
+		}
+		
+	} catch(e) {
+		console.error('遠端資料存取錯誤:', e);
+	}
+		
+	
+};
+
+
 
 // 執行 fetchRooms()
 fetchRooms();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
