5dbd6563900f2b19aff9ed84fb1872cfa1777819
NTPU <NTPU@503-tr>
2024-11-15 11:11:16 +0800

    w

--- react-app/my-react-2/src/App3.jsx
+++ react-app/my-react-2/src/App3.jsx
@@ -1,35 +1,35 @@
 import { useState } from 'react';
 import './App.css'
 
-const MyInput = () => {
+const MyInput = ({setMessage}) => {
+  
+  const handleChange = (e) => {
+    setMessage(e.target.value); // 透過 setMessage 變更 message 變數內容後網頁會自動渲染更新
+    console.log('e.target.value:', e.target.value);
+  };
+
   return (
     <div>
-      <input type="text" placeholder='請輸入一些內容' />
+      <input type="text" placeholder='請輸入一些內容' onChange={handleChange} />
     </div>
   );
 };
 
-const MyDisplay = () => {
+const MyDisplay = ({message}) => {
   return (
     <div>
-      顯示:
+      顯示: {message}
     </div> 
   );
 };
 
-
 function App() {
   const [message, setMessage] = useState(''); // '' 表示 message 的初始值
 
-  const handleChange = (e) => {
-    setMessage(e.target.value); // 透過 setMessage 變更 message 變數內容後網頁會自動渲染更新
-    console.log('message:', message);
-  };
-
   return (
     <>
-      <MyInput />
-      <MyDisplay />
+      <MyInput setMessage={setMessage} />
+      <MyDisplay message={message} />
     </>
   )
 }
