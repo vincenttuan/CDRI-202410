ac72a28bccb79e87ba5abcdc6a4c25cccaaaf48e
NTPU <NTPU@503-tr>
2024-11-18 13:40:32 +0800

    web

--- springboot-csr-todolist-react/src/main/java/com/example/todolist/exception/TodoNotFoundException.java
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/exception/TodoNotFoundException.java
@@ -1,6 +1,6 @@
 package com.example.todolist.exception;
 
-// 自訂例外
+// 自訂例外(受檢例外)
 public class TodoNotFoundException extends Exception {
 	public TodoNotFoundException(String message) {
 		super(message);
--- springboot-csr-todolist-react/src/main/java/com/example/todolist/service/TodoService.java
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/service/TodoService.java
@@ -2,6 +2,7 @@ package com.example.todolist.service;
 
 import java.util.List;
 
+import com.example.todolist.exception.TodoNotFoundException;
 import com.example.todolist.model.dto.TodoDTO;
 
 public interface TodoService {
@@ -13,9 +14,8 @@ public interface TodoService {
 	public TodoDTO createTodo(TodoDTO todoDTO);
 	
 	// 更新代辦事項
-	public TodoDTO updateTodo(TodoDTO todoDTO);
+	public TodoDTO updateTodo(TodoDTO todoDTO) throws TodoNotFoundException;
 	
 	// 刪除代辦事項
-	public void deleteTodo(Long id);
-	
+	public void deleteTodo(Long id) throws TodoNotFoundException;
 }
