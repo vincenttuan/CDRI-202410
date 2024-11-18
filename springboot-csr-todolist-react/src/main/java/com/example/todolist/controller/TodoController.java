443dd028482fe96077a91516eb4fa3bf2c5a9a5e
NTPU <NTPU@503-tr>
2024-11-18 13:43:34 +0800

    web

--- springboot-csr-todolist-react/src/main/java/com/example/todolist/controller/TodoController.java
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/controller/TodoController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.todolist.exception.TodoNotFoundException;
 import com.example.todolist.model.dto.TodoDTO;
 import com.example.todolist.response.ApiResponse;
 import com.example.todolist.service.TodoService;
@@ -57,7 +58,7 @@ public class TodoController {
 	
 	// 更新待辦事項
 	@PutMapping("/{id}")
-	public ResponseEntity<ApiResponse<TodoDTO>> updateTodo(@PathVariable Long id, @RequestBody TodoDTO todoDto) {
+	public ResponseEntity<ApiResponse<TodoDTO>> updateTodo(@PathVariable Long id, @RequestBody TodoDTO todoDto) throws TodoNotFoundException {
 		todoDto.setId(id);
 		TodoDTO updatedTodoDTO = todoService.updateTodo(todoDto);
 		return ResponseEntity.ok(ApiResponse.success("修改成功", updatedTodoDTO));
@@ -65,14 +66,14 @@ public class TodoController {
 	
 	// 刪除待辦事項
 	@DeleteMapping("/{id}")
-	public ResponseEntity<ApiResponse<Void>> deleteTodo(@PathVariable Long id) {
+	public ResponseEntity<ApiResponse<Void>> deleteTodo(@PathVariable Long id) throws TodoNotFoundException {
 		todoService.deleteTodo(id);
 		return ResponseEntity.ok(ApiResponse.success("刪除成功", null));
 	}
 	
-	// 處理異常狀況
-	@ExceptionHandler(RuntimeException.class)
-	public ResponseEntity<ApiResponse<Void>> handlTodoRuntimeException(RuntimeException e) {
+	// 統一處理異常狀況
+	@ExceptionHandler(TodoNotFoundException.class)
+	public ResponseEntity<ApiResponse<Void>> handlTodoRuntimeException(TodoNotFoundException e) {
 		return ResponseEntity.status(HttpStatus.NOT_FOUND)
 				.body(ApiResponse.error(HttpStatus.NOT_FOUND.value(), e.getMessage()));
 	}
