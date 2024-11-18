bd943a7fcfde6994972912066ea489f226d0736a
NTPU <NTPU@503-tr>
2024-11-18 11:06:21 +0800

    web

---
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/SpringbootCsrTodolistReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCsrTodolistReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCsrTodolistReactApplication.class, args);
+	}
+
+}
