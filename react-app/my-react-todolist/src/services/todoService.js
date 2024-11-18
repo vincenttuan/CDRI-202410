c3669238f4d39967bcec5a82c48e4bd453deb35d
NTPU <NTPU@503-tr>
2024-11-18 14:12:25 +0800

    web

--- react-app/my-react-todolist/src/services/todoService.js
+++ react-app/my-react-todolist/src/services/todoService.js
@@ -34,3 +34,31 @@ export const addTodo = async(todo) => {
     }
     throw new Error(result.message);
 };
+
+// 更新待辦事項
+export const updateTodo = async(updateTodo) => {
+    const response = await fetch(`${BASE_URL}/${updateTodo.id}`, {
+        method: 'PUT',
+        headers: {
+            'Content-type': 'application/json';
+        },
+        body: JSON.stringify(updateTodo),
+    });
+    const result = await response.json();
+    if (result.status === 200) {
+        return result.data; // 返回資料
+    }
+    throw new Error(result.message);
+};
+
+// 刪除待辦事項
+export const deleteTodo = async(id) => {
+    const response = await fetch(`${BASE_URL}/${id}`, {
+        method: 'DELETE',
+    });
+    const result = await response.json();
+    if (result.status === 200) {
+        return true; // 返回 true
+    }
+    throw new Error(result.message);
+};
