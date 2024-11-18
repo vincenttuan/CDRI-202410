5d7f708cd6abc08bb4e29a84bc088b87c97b0829
NTPU <NTPU@503-tr>
2024-11-18 12:32:13 +0800

    web

--- springboot-csr-todolist-react/src/main/java/com/example/todolist/controller/TodoController.java
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/controller/TodoController.java
@@ -3,9 +3,12 @@ package com.example.todolist.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -66,5 +69,14 @@ public class TodoController {
 		todoService.deleteTodo(id);
 		return ResponseEntity.ok(ApiResponse.success("刪除成功", null));
 	}
+	
+	// 處理異常狀況
+	@ExceptionHandler(RuntimeException.class)
+	public ResponseEntity<ApiResponse<Void>> handlTodoRuntimeException(RuntimeException e) {
+		return ResponseEntity.status(HttpStatus.NOT_FOUND)
+				.body(ApiResponse.error(HttpStatus.NOT_FOUND.value(), e.getMessage()));
+	}
+	
+	
 	
 }
