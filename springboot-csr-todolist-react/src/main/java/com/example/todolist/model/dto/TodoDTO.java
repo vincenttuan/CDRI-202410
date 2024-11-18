bc594593fc73f58fcf66abd28363ef1678d97ae1
NTPU <NTPU@503-tr>
2024-11-18 11:21:46 +0800

    web

--- springboot-csr-todolist-react/src/main/java/com/example/todolist/model/dto/TodoDTO.java
+++ springboot-csr-todolist-react/src/main/java/com/example/todolist/model/dto/TodoDTO.java
@@ -1,5 +1,14 @@
 package com.example.todolist.model.dto;
 
-public class TodoDTO {
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TodoDTO {
+	private Long id;
+	private String text;
+	private Boolean completed;
 }
