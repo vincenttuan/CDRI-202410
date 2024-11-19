8659d6b958ee00b30fa5286b61e2a10494c1722b
NTPU <NTPU@503-tr>
2024-11-19 10:11:39 +0800

    web

---
+++ springboot-tx/src/main/java/com/example/tx/entity/Book.java
@@ -0,0 +1,19 @@
+package com.example.tx.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Book {
+	@Id
+	private Integer id;
+	private String title;
+	private Integer price;
+	
+}
