8a1a6fa7b7ed5afa26e512a087629c06582f8a56
NTPU <NTPU@503-tr>
2024-11-19 10:15:19 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/entity/Wallet.java
+++ springboot-tx/src/main/java/com/example/tx/entity/Wallet.java
@@ -2,8 +2,14 @@ package com.example.tx.entity;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Wallet {
 	
 	@Id
