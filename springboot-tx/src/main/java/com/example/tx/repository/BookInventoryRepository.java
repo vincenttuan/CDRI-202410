430c54aa1429e16543acc6e56721277bd5951b16
NTPU <NTPU@503-tr>
2024-11-19 10:56:08 +0800

    web

--- springboot-tx/src/main/java/com/example/tx/repository/BookInventoryRepository.java
+++ springboot-tx/src/main/java/com/example/tx/repository/BookInventoryRepository.java
@@ -12,7 +12,7 @@ public interface BookInventoryRepository extends JpaRepository<BookInventory, In
 	@Query(value = "select book_amount from book_inventory where book_id = :bookId", nativeQuery = true)
 	Integer getBookAmount(Integer bookId);
 	
-	// 更新庫存
+	// 更新庫存(目前存量 - amountToReduce)
 	@Query(value = "update book_inventory set book_amount = book_amount - :amountToReduce where book_id = :bookId", nativeQuery = true)
 	void updateBookAmount(Integer amountToReduce, Integer bookId);
 	
