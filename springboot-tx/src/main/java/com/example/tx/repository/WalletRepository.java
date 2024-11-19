5510423ebf6ac7c65483d82e52179fb9401dfa09
NTPU <NTPU@503-tr>
2024-11-19 10:33:07 +0800

    web

---
+++ springboot-tx/src/main/java/com/example/tx/repository/WalletRepository.java
@@ -0,0 +1,14 @@
+package com.example.tx.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.tx.entity.Wallet;
+
+@Repository
+public interface WalletRepository extends JpaRepository<Wallet, String> {
+	// 取得客戶餘額
+	@Query(value = "select balance from wallet where username = :username", nativeQuery = true)
+	Integer getWalletBalance(String username);
+}
