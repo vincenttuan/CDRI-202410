45f538b17ca3ec6b0f76477528d1cc607f925b80
NTPU <NTPU@503-tr>
2024-11-20 11:44:08 +0800

    web

---
+++ springboot-cart/src/test/java/com/example/cart/SpringbootCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
