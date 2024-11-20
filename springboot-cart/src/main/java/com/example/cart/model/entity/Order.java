7464a7532c8035f22fac14968ff0f2550dae3c41
NTPU <NTPU@503-tr>
2024-11-20 13:46:35 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/model/entity/Order.java
+++ springboot-cart/src/main/java/com/example/cart/model/entity/Order.java
@@ -1,11 +1,14 @@
 package com.example.cart.model.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -22,6 +25,10 @@ public class Order {
 	@ManyToOne
 	@JoinColumn(name = "user_id")
 	private User user;
+	
+	// order 與 order_item 的關係是一對多
+	@OneToMany(mappedBy = "order")
+	private List<OrderItem> items;
 	
 }
 
--- springboot-cart/src/main/java/com/example/cart/model/entity/OrderItem.java
+++ springboot-cart/src/main/java/com/example/cart/model/entity/OrderItem.java
@@ -1,5 +1,39 @@
 package com.example.cart.model.entity;
 
-public class OrderItem {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "order_item")
+public class OrderItem {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	// 商品數量
+	private int quantity;
+	
+	// order_item 與 order 的關係是多對一關聯
+	@ManyToOne
+	@JoinColumn(name = "order_id")
+	private Order order;
+	
+	
 }
+
+
+
+
+
+
+
+
+
