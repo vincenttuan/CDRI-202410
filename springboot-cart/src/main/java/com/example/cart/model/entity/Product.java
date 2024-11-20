fdd4cda03c0079acabd1b5e23a7bce260079e017
NTPU <NTPU@503-tr>
2024-11-20 14:18:05 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/model/entity/Product.java
+++ springboot-cart/src/main/java/com/example/cart/model/entity/Product.java
@@ -1,9 +1,12 @@
 package com.example.cart.model.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -19,6 +22,8 @@ public class Product {
 	
 	private Integer price;
 	
+	@OneToMany(mappedBy = "product")
+	private List<OrderItem> orderItems;
 	
 }
 
