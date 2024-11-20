92777e9f8c056cfe83859853d2c8fb8fc534527b
NTPU <NTPU@503-tr>
2024-11-20 16:06:42 +0800

    web

--- springboot-cart/src/test/java/com/example/cart/AddUsersTests.java
+++ springboot-cart/src/test/java/com/example/cart/AddUsersTests.java
@@ -17,14 +17,12 @@ class AddUsersTests {
 		User user1 = new User();
 		user1.setUsername("admin");
 		user1.setPassword("1234");
+		userRepository.save(user1);
 		
 		User user2 = new User();
 		user2.setUsername("mary");
 		user2.setPassword("1234");
-		
-		userRepository.save(user1);
 		userRepository.save(user2);
-		
 		
 	}
 
