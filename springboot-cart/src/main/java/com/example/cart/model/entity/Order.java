dc8851733f15b59cd59fe3186468a296663038cb
NTPU <NTPU@503-tr>
2024-11-20 12:09:23 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/model/entity/Order.java
+++ springboot-cart/src/main/java/com/example/cart/model/entity/Order.java
@@ -1,5 +1,27 @@
 package com.example.cart.model.entity;
 
-public class Order {
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "`order`")
+public class Order {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	
 }
+
+
+
+
+
+
