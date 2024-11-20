99edaec4cfcc41bc73fbe3c845e271d24ccf9ac3
NTPU <NTPU@503-tr>
2024-11-20 14:20:46 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/model/entity/ProductImage.java
+++ springboot-cart/src/main/java/com/example/cart/model/entity/ProductImage.java
@@ -1,5 +1,23 @@
 package com.example.cart.model.entity;
 
-public class ProductImage {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
+@Entity
+@Table(name = "product_image")
+public class ProductImage {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(length = 4000)
+	private String imageBase64;
+	
+	
 }
