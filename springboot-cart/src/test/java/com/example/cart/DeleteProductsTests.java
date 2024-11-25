62742d8f6ee154b56c1ca659ff8e4c87bc85d426
NTPU <NTPU@503-tr>
2024-11-25 09:53:32 +0800

    web

--- springboot-cart/src/test/java/com/example/cart/DeleteProductsTests.java
+++ springboot-cart/src/test/java/com/example/cart/DeleteProductsTests.java
@@ -9,6 +9,9 @@ import com.example.cart.model.entity.ProductImage;
 import com.example.cart.repository.ProductImageRepository;
 import com.example.cart.repository.ProductRepository;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.OneToOne;
+
 @SpringBootTest
 class DeleteProductsTests {
     @Autowired
@@ -18,6 +21,8 @@ class DeleteProductsTests {
     void add() {
         Product product = productRepository.findById(2L).get();
     	// 移除 Product，同時也會移除 ProductImage
+        // 因為 Product 與 ProductImage 的一對一關聯 (單向) 有設定聯及操作 
+        // @OneToOne(cascade = CascadeType.ALL)
         productRepository.delete(product);
         
     }
