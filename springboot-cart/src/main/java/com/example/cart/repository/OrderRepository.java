1ad5bb88d31d36fafce6dc843237cf05d30d40a9
NTPU <NTPU@503-tr>
2024-11-25 10:10:13 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/repository/OrderRepository.java
+++ springboot-cart/src/main/java/com/example/cart/repository/OrderRepository.java
@@ -1,5 +1,7 @@
 package com.example.cart.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.example.cart.model.entity.Order;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
-
+	// 取得該使用者的訂單紀錄
+	List<Order> findByUserId(Long userId);
 }
