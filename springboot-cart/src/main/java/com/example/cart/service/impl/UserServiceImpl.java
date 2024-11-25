82f53f62f374a97e6070d3079bc9774a87808f63
NTPU <NTPU@503-tr>
2024-11-25 10:39:49 +0800

    web

---
+++ springboot-cart/src/main/java/com/example/cart/service/impl/UserServiceImpl.java
@@ -0,0 +1,51 @@
+package com.example.cart.service.impl;
+
+import java.util.Optional;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cart.model.dto.LoginDTO;
+import com.example.cart.model.dto.UserDTO;
+import com.example.cart.model.entity.User;
+import com.example.cart.repository.UserRepository;
+import com.example.cart.service.UserService;
+
+@Service
+public class UserServiceImpl implements UserService {
+	
+	@Autowired
+	private UserRepository userRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
+	@Override
+	public Optional<UserDTO> findByUsername(String username) {
+		Optional<User> optUser = userRepository.findByUsername(username);
+		if(optUser.isEmpty()) Optional.empty();
+		// 利用 modelMapper 將 User 轉 UserDTO
+		UserDTO userDTO = modelMapper.map(optUser.get(), UserDTO.class);
+		return Optional.of(userDTO);
+	}
+
+	@Override
+	public Optional<UserDTO> login(LoginDTO loginDTO) {
+		Optional<User> optUser = userRepository.findByUsername(loginDTO.getUsername());
+		// 判斷密碼
+		if(optUser.isPresent() && optUser.get().getPassword().equals(loginDTO.getPassword())) {
+			return Optional.of(modelMapper.map(optUser.get(), UserDTO.class));
+		} 
+		return Optional.empty();
+	}
+
+	@Override
+	public Optional<UserDTO> saveUser(UserDTO userDTO) {
+		User user = modelMapper.map(userDTO, User.class);
+		userRepository.save(user);
+		
+		return Optional.of(modelMapper.map(user, UserDTO.class));
+	}
+	
+}
