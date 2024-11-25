33495bc1cb5bc55a1366dd67fd9457b4a4597c5a
NTPU <NTPU@503-tr>
2024-11-25 15:10:15 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/controller/ProductController.java
+++ springboot-cart/src/main/java/com/example/cart/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.example.cart.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -34,6 +36,17 @@ public class ProductController {
 	public ResponseEntity<ApiResponse<List<ProductDTO>>> getAllProducts() {
 		return ResponseEntity.ok(ApiResponse.success("查詢成功", productService.getAllProducts()));
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ApiResponse<ProductDTO>> getProduct(@PathVariable Long id) {
+		Optional<ProductDTO> optProductDTO = productService.getProductById(id);
+		if(optProductDTO.isEmpty()) {
+			return ResponseEntity.status(404).body(ApiResponse.error(404, "查無此商品"));
+		}
+		return ResponseEntity.ok(ApiResponse.success("查詢成功", optProductDTO.get()));
+	}
+	
+	
 	
 	
 }
