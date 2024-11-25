dafbf6644445d544d0526d9d1efa931e164b732e
NTPU <NTPU@503-tr>
2024-11-25 11:52:36 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/service/impl/OrderServiceImpl.java
+++ springboot-cart/src/main/java/com/example/cart/service/impl/OrderServiceImpl.java
@@ -1,20 +1,31 @@
 package com.example.cart.service.impl;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.example.cart.model.dto.OrderDTO;
 import com.example.cart.model.dto.OrderItemDTO;
+import com.example.cart.repository.OrderRepository;
 import com.example.cart.service.OrderService;
 
 @Service
 public class OrderServiceImpl implements OrderService {
-
+	
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	@Autowired
+	private ModelMapper modelMapper;
+	
 	@Override
 	public List<OrderDTO> findOrdersByUserId(Long userId) {
-		// TODO Auto-generated method stub
-		return null;
+		return orderRepository.findByUserId(userId).stream() // ... Order
+				.map(order -> modelMapper.map(order, OrderDTO.class)) // ... OrderDTO
+				.collect(Collectors.toList());
 	}
 
 	@Override
