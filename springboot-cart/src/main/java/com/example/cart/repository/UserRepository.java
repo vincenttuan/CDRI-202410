f9da13330001b0e68c9d90eb5e1d1c3ba2c11387
NTPU <NTPU@503-tr>
2024-11-25 10:08:33 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/repository/UserRepository.java
+++ springboot-cart/src/main/java/com/example/cart/repository/UserRepository.java
@@ -1,11 +1,15 @@
 package com.example.cart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.example.cart.model.entity.User;
+import java.util.List;
+
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
+	Optional<User> findByUsername(String username);
 }
