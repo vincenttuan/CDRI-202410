1664c6be180c7f54655e22f605fed78a72f87a99
NTPU <NTPU@503-tr>
2024-11-25 10:13:59 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/model/dto/LoginDTO.java
+++ springboot-cart/src/main/java/com/example/cart/model/dto/LoginDTO.java
@@ -1,5 +1,8 @@
 package com.example.cart.model.dto;
 
+import lombok.Data;
+
+@Data
 public class LoginDTO {
 	private String username;
 	private String password;
---
+++ springboot-cart/src/main/java/com/example/cart/model/dto/UserDTO.java
@@ -0,0 +1,9 @@
+package com.example.cart.model.dto;
+
+import lombok.Data;
+
+@Data
+public class UserDTO {
+	private Long id;
+	private String username;
+}
