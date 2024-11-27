ebe4ecd1318cdf649a1eb3c5c6573014bae5d924
NTPU <NTPU@503-tr>
2024-11-27 10:37:06 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/aop/UserSessionCheckAspect.java
+++ springboot-cart/src/main/java/com/example/cart/aop/UserSessionCheckAspect.java
@@ -1,5 +1,15 @@
 package com.example.cart.aop;
 
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.example.cart.exception.UnauthorizedException;
+import com.example.cart.model.dto.UserDTO;
+
+import jakarta.servlet.http.HttpSession;
+
 /**
  * 切面類別：UserSessionCheckAspect
  *
@@ -20,6 +30,30 @@ package com.example.cart.aop;
  * 使用方式：
  * - 方法上加上 @CheckUserSession 註解，AOP 會自動攔截並檢查。
  * */
+@Aspect // 宣告此類別為 AOP 切面
+@Component // 由 Spring 來管理此物件
 public class UserSessionCheckAspect {
+	@Autowired
+	private HttpSession session; // 自動注入 HttpSession
+	
+	@Before("@annotation(com.example.cart.aop.CheckUserSession)")
+	public void checkUserSession() throws UnauthorizedException {
+		// 取得 user 資訊
+		UserDTO userDTO = (UserDTO)session.getAttribute("userDTO");
+		// 檢查用戶是否已經登入
+		if(userDTO == null) {
+			// 未登入, 拋出未授權例外
+			throw new UnauthorizedException("未登入或登入已過期");
+		}
+	}
 	
 }
+
+
+
+
+
+
+
+
+
