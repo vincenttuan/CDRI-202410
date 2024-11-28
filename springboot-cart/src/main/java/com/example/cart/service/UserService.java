66df11eb5f077725ae610cc39264e3d05690e8ce
NTPU <NTPU@503-tr>
2024-11-28 09:57:37 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/service/UserService.java
+++ springboot-cart/src/main/java/com/example/cart/service/UserService.java
@@ -3,6 +3,8 @@ package com.example.cart.service;
 import java.util.List;
 import java.util.Optional;
 
+import com.example.cart.model.dto.FavoriteProductDTO;
+import com.example.cart.model.dto.FavoriteUserDTO;
 import com.example.cart.model.dto.LoginDTO;
 import com.example.cart.model.dto.UserDTO;
 import com.example.cart.model.entity.Product;
@@ -11,6 +13,12 @@ public interface UserService {
 	Optional<UserDTO> findByUsername(String username);
 	Optional<UserDTO> login(LoginDTO loginDTO);
 	Optional<UserDTO> saveUser(UserDTO userDTO);
+	
+	// 用戶關注列表(用戶關注那些商品)
+	public List<FavoriteProductDTO> getFavoriteProducts(Long userId);
+	
+	// 商品關注列表(商品被那些用戶關注)
+	public List<FavoriteUserDTO> getFavoriteUsers(Long productId);
 	
 	
 }
