4d9da0c695620f9771348eea388d30ce3b70fb00
NTPU <NTPU@503-tr>
2024-11-28 11:54:28 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/exception/GlobalExceptionHandler.java
+++ springboot-cart/src/main/java/com/example/cart/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.example.cart.exception;
 
+import java.lang.reflect.UndeclaredThrowableException;
 import java.util.Locale;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,13 +18,22 @@ public class GlobalExceptionHandler {
 	@Autowired
 	private MessageSource messageSource;
 	
-	@ExceptionHandler({UnauthorizedException.class, ProductNotFoundException.class, UserNotFoundException.class})
+	
+	@ExceptionHandler(Exception.class)
 	// Locale 在 Spring 中或預設會參考瀏覽器的 Accept-Language Header 來判斷
 	// 範例: accept-language header: zh-CN, zh-TW; q = ...
 	public ResponseEntity<ApiResponse<String>> handleUnauthorizedException(Exception ex, Locale locale) {
+		//ex.printStackTrace();
+		
+		Throwable actualException = ex;
+
+	    // 解包 UndeclaredThrowableException
+	    if (ex instanceof UndeclaredThrowableException) {
+	        actualException = ((UndeclaredThrowableException) ex).getUndeclaredThrowable();
+	    }
+	    
 		String message;
 	    HttpStatus status;
-
 	    if (ex instanceof UnauthorizedException) {
 	        message = ex.getMessage().isEmpty() ? messageSource.getMessage("exception.unauthorized", null, locale) : ex.getMessage();
 	        status = HttpStatus.FORBIDDEN;
@@ -34,7 +44,7 @@ public class GlobalExceptionHandler {
 	        message = ex.getMessage().isEmpty() ? messageSource.getMessage("exception.user_not_found", null, locale) : ex.getMessage();
 	        status = HttpStatus.NOT_FOUND;
 	    } else {
-	        message = ex.getMessage();
+	    	message = actualException.getMessage();
 	        status = HttpStatus.INTERNAL_SERVER_ERROR;
 	    }
 
