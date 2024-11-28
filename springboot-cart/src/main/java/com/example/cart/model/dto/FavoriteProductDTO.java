f2ded245f8d70a197bbebd65cf5e3b7430ce7066
NTPU <NTPU@503-tr>
2024-11-28 09:53:49 +0800

    web

---
+++ springboot-cart/src/main/java/com/example/cart/model/dto/FavoriteProductDTO.java
@@ -0,0 +1,9 @@
+package com.example.cart.model.dto;
+
+import lombok.Data;
+
+@Data
+public class FavoriteProductDTO {
+	private Long id;
+	private String name;
+}
---
+++ springboot-cart/src/main/java/com/example/cart/model/dto/FavoriteUserDTO.java
@@ -0,0 +1,6 @@
+package com.example.cart.model.dto;
+
+public class FavoriteUserDTO {
+	private Long id;
+	private String username;
+}
