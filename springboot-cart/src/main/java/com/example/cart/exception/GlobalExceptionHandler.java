dfc728d13ee03c8748d6ee338899bc4c2f463a2e
NTPU <NTPU@503-tr>
2024-11-28 10:32:18 +0800

    web

--- springboot-cart/src/main/java/com/example/cart/exception/GlobalExceptionHandler.java
+++ springboot-cart/src/main/java/com/example/cart/exception/GlobalExceptionHandler.java
@@ -1,5 +1,9 @@
 package com.example.cart.exception;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -10,19 +14,22 @@ import com.example.cart.response.ApiResponse;
 @ControllerAdvice // 可以用來處理全域的例外
 public class GlobalExceptionHandler {
 	
+	@Autowired
+	private MessageSource messageSource;
+	
 	@ExceptionHandler({UnauthorizedException.class, ProductNotFoundException.class, UserNotFoundException.class})
-	public ResponseEntity<ApiResponse<String>> handleUnauthorizedException(Exception ex) {
+	public ResponseEntity<ApiResponse<String>> handleUnauthorizedException(Exception ex, Locale locale) {
 		String message;
 	    HttpStatus status;
 
 	    if (ex instanceof UnauthorizedException) {
-	        message = ex.getMessage().isEmpty() ? "未登入或登入失敗" : ex.getMessage();
+	        message = ex.getMessage().isEmpty() ? messageSource.getMessage("exception.unauthorized", null, locale) : ex.getMessage();
 	        status = HttpStatus.FORBIDDEN;
 	    } else if (ex instanceof ProductNotFoundException) {
-	        message = ex.getMessage().isEmpty() ? "商品不存在" : ex.getMessage();
+	        message = ex.getMessage().isEmpty() ? messageSource.getMessage("exception.product_not_found", null, locale) : ex.getMessage();
 	        status = HttpStatus.NOT_FOUND;
 	    } else if (ex instanceof UserNotFoundException) {
-	        message = ex.getMessage().isEmpty() ? "用戶不存在" : ex.getMessage();
+	        message = ex.getMessage().isEmpty() ? messageSource.getMessage("exception.user_not_found", null, locale) : ex.getMessage();
 	        status = HttpStatus.NOT_FOUND;
 	    } else {
 	        message = ex.getMessage();
