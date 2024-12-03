c445d5be7eb2136d1a431e151aad1b904a256794
NTPU <NTPU@503-tr>
2024-12-03 09:55:29 +0800

    web

---
+++ springboot-proxy-aop/src/main/java/com/example/proxy/SpringbootProxyAopApplication.java
@@ -0,0 +1,13 @@
+package com.example.proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootProxyAopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootProxyAopApplication.class, args);
+	}
+
+}
