e5466ac995751a6340d78a1375299c35759b3a35
NTPU <NTPU@503-tr>
2024-12-03 10:12:19 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,8 @@
 package com.example.proxy.service.impl;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.springframework.stereotype.Service;
 
 import com.example.proxy.service.PaymentService;
@@ -8,13 +11,35 @@ import com.example.proxy.service.PaymentService;
 public class PaymentServiceImpl implements PaymentService {
 
 	@Override
-	public void pay(int amount) {
+	public void pay(int amount) { // 付款方法(業務方法)
+		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
+		if(amount <= 0) {
+			System.out.println("支付失敗: 金額必須大於零!");
+			return;
+		}
+		
+		// 業務邏輯
 		System.out.println("支付 " + amount + " 元");
+		
+		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("支付 " + amount + " 元 " + sdf.format(new Date()));
 	}
 
 	@Override
-	public void refund(int amount) {
+	public void refund(int amount) { // 退款方法(業務方法)
+		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
+		if(amount <= 0) {
+			System.out.println("支付失敗: 金額必須大於零!");
+			return;
+		}
+		
+		// 業務邏輯
 		System.out.println("退款 " + amount + " 元");
+		
+		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("支付 " + amount + " 元 " + sdf.format(new Date()));
 	}
 
 }
