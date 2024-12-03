acf51a2c800a179e2c54e354e00d0c519d271e80
NTPU <NTPU@503-tr>
2024-12-03 11:28:59 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/TransactionAspect.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/TransactionAspect.java
@@ -4,20 +4,32 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TransactionAspect {
+	// 切點方法
+	@Pointcut(value = "execution(* com.example.proxy.service.TransactionService.pay(..))")
+	public void pay() {};
 	
-	@Before("execution(* com.example.proxy.service.TransactionService.pay(..))")
+	@Pointcut(value = "execution(* com.example.proxy.service.TransactionService.refund(..))")
+	public void refund() {};
+	
+	@Before(value = "pay() || refund()")
 	public void checkAmount(JoinPoint joinPoint) {
-		System.out.println("Before pay ...");
+		System.out.println("Before ...");
 	}
 	
-	@AfterReturning("execution(* com.example.proxy.service.TransactionService.pay(..))")
+	@AfterReturning(value = "pay()")
 	public void logPay(JoinPoint joinPoint) {
 		System.out.println("After pay ...");
+	}
+	
+	@AfterReturning(value = "refund()")
+	public void logRefund(JoinPoint joinPoint) {
+		System.out.println("After refund ...");
 	}
 	
 }
