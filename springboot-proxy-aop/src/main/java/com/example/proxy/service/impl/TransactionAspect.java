5d1b82625cdae4c64a0398d653557be1d9bbe6cc
NTPU <NTPU@503-tr>
2024-12-03 12:15:24 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/TransactionAspect.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/TransactionAspect.java
@@ -9,11 +9,16 @@ import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class TransactionAspect {
+	// 建立 Logger 實例
+	private static final Logger logger = LoggerFactory.getLogger(TransactionAspect.class);
+	
 	// 切點方法
 	@Pointcut(value = "execution(* com.example.proxy.service.TransactionService.pay(..))")
 	public void pay() {};
