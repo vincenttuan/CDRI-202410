204cdb191e75e081cae234dcb1639be35a7f6f85
NTPU <NTPU@503-tr>
2024-12-03 09:59:27 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -1,7 +1,10 @@
 package com.example.proxy.service.impl;
 
+import org.springframework.stereotype.Service;
+
 import com.example.proxy.service.PaymentService;
 
+@Service
 public class PaymentServiceImpl implements PaymentService {
 
 	@Override
