1e9694c95ecaf71a4702b187a9b75ec03dc7d68a
NTPU <NTPU@503-tr>
2024-12-03 10:43:42 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -1,5 +1,6 @@
 package com.example.proxy.service.impl;
 
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Service;
 import com.example.proxy.service.PaymentService;
 
--- springboot-proxy-aop/src/test/java/com/example/proxy/TestPaymentService.java
+++ springboot-proxy-aop/src/test/java/com/example/proxy/TestPaymentService.java
@@ -10,27 +10,26 @@ import com.example.proxy.service.impl.PaymentServiceProxy;
 @SpringBootTest
 public class TestPaymentService {
 	
+	// 會被強迫使用代理程式 PaymentServiceProxy
 	@Autowired
-	private PaymentService paymentService;
+	private PaymentService paymentService; 
 	
 	@Test
 	public void test() {
-		// 直接調用業務方法
-		/*
-		paymentService.pay(100);
-		paymentService.pay(-50);
-		
-		paymentService.refund(200);
-		paymentService.refund(-30);
-		*/
-		
 		// 透過代理物件來調用業務方法
+		/*
 		PaymentServiceProxy proxy = new PaymentServiceProxy(paymentService);
 		proxy.pay(100);
 		proxy.pay(-50);
 		
 		proxy.refund(200);
 		proxy.refund(-30);
+		*/
+		paymentService.pay(100);
+		paymentService.pay(-50);
+		
+		paymentService.refund(200);
+		paymentService.refund(-30);
 		
 	}
 	
