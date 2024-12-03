e594e6be25c551a66c502b21a3f60b3662099a79
NTPU <NTPU@503-tr>
2024-12-03 10:21:31 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -1,10 +1,6 @@
 package com.example.proxy.service.impl;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 import org.springframework.stereotype.Service;
-
 import com.example.proxy.service.PaymentService;
 
 @Service
@@ -12,34 +8,14 @@ public class PaymentServiceImpl implements PaymentService {
 
 	@Override
 	public void pay(int amount) { // 付款方法(業務方法)
-		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
-		if(amount <= 0) {
-			System.out.println("支付失敗: 金額必須大於零!");
-			return;
-		}
-		
 		// 業務邏輯
 		System.out.println("支付 " + amount + " 元");
-		
-		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("支付 " + amount + " 元 " + sdf.format(new Date()));
 	}
 
 	@Override
 	public void refund(int amount) { // 退款方法(業務方法)
-		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
-		if(amount <= 0) {
-			System.out.println("退款失敗: 金額必須大於零!");
-			return;
-		}
-		
 		// 業務邏輯
 		System.out.println("退款 " + amount + " 元");
-		
-		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
-		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("退款 " + amount + " 元 " + sdf.format(new Date()));
 	}
 
 }
---
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceProxy.java
@@ -0,0 +1,49 @@
+package com.example.proxy.service.impl;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import com.example.proxy.service.PaymentService;
+
+
+public class PaymentServiceProxy implements PaymentService {
+	
+	private PaymentService paymentService;
+	
+	public PaymentServiceProxy(PaymentService paymentService) {
+		this.paymentService = paymentService;
+	}
+	
+	@Override
+	public void pay(int amount) {
+		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
+		if(amount <= 0) {
+			System.out.println("支付失敗: 金額必須大於零!");
+			return;
+		}
+		
+		// 執行實際支付業務
+		paymentService.pay(amount); 
+		
+		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("支付 " + amount + " 元 " + sdf.format(new Date()));
+	}
+
+	@Override
+	public void refund(int amount) {
+		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
+		if(amount <= 0) {
+			System.out.println("退款失敗: 金額必須大於零!");
+			return;
+		}
+		
+		// 執行實際退款業務
+		paymentService.refund(amount);
+		
+		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		System.out.println("退款 " + amount + " 元 " + sdf.format(new Date()));
+	}
+
+}
