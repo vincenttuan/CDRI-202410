ca9a39a1e3d3eb834c846a9816f0be512969aee3
NTPU <NTPU@503-tr>
2024-12-03 10:12:41 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/PaymentServiceImpl.java
@@ -30,7 +30,7 @@ public class PaymentServiceImpl implements PaymentService {
 	public void refund(int amount) { // 退款方法(業務方法)
 		// Before 執行業務邏輯之前:檢查金額 (公用邏輯)
 		if(amount <= 0) {
-			System.out.println("支付失敗: 金額必須大於零!");
+			System.out.println("退款失敗: 金額必須大於零!");
 			return;
 		}
 		
@@ -39,7 +39,7 @@ public class PaymentServiceImpl implements PaymentService {
 		
 		// After 執行業務邏輯之後:添加日誌 (公用邏輯)
 		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-		System.out.println("支付 " + amount + " 元 " + sdf.format(new Date()));
+		System.out.println("退款 " + amount + " 元 " + sdf.format(new Date()));
 	}
 
 }
