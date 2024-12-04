e8e94cda4b37627c24ee17fe9b477bd80dcdb71a
NTPU <NTPU@503-tr>
2024-12-04 12:00:42 +0800

    aop

--- springboot-csr-cart/src/main/java/com/example/cart/aop/LogAspect.java
+++ springboot-csr-cart/src/main/java/com/example/cart/aop/LogAspect.java
@@ -3,6 +3,10 @@ package com.example.cart.aop;
 // 針對結帳進行 log 紀錄
 // OrderService.saveOrder
 // 隨堂練習
+// 加分項: log 檔案會自動更替日期名稱
+// 例如: 2024-12-04是: order.log <- 今日
+//      2024-12-05是: 2024-12-04-order.log <- 昨日的 order.log 會自動更名變成 2024-12-04-order.log
+//                    : order.log <- 今日
 public class LogAspect {
 
 }
