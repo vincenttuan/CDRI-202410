754b509769ed94eb764cea3b9024c68242efdcdc
NTPU <NTPU@503-tr>
2024-12-04 10:09:08 +0800

    web

---
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/BMIServiceAspect.java
@@ -0,0 +1,29 @@
+package com.example.proxy.service.impl;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class BMIServiceAspect {
+	
+	// 建立 Logger 實例
+	private static final Logger logger = LoggerFactory.getLogger(BMIServiceAspect.class);
+		
+	// 定義切點
+	@Pointcut("execution(* *.BMIService.*(..)) && args(h, w)")
+	public void pt() {}
+
+	// BMI 調用紀錄
+	// 前置通知
+	@Before(value = "pt()")
+	public void before(Double h, Double w) {
+		logger.info("h={}, w={}", h, w);
+	}
+	
+}
