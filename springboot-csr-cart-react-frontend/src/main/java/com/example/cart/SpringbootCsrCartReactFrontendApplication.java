826173ad0895d427e80212da48d9a69d2f9b6fef
NTPU <NTPU@503-tr>
2024-12-04 15:32:50 +0800

    web

---
+++ springboot-csr-cart-react-frontend/src/main/java/com/example/cart/SpringbootCsrCartReactFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.example.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCsrCartReactFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootCsrCartReactFrontendApplication.class, args);
+	}
+
+}
