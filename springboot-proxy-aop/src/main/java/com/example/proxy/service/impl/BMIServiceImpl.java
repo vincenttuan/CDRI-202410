0b8ee9307b77f762d8876a3df160c8c23e949fce
NTPU <NTPU@503-tr>
2024-12-04 09:58:53 +0800

    web

--- springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/BMIServiceImpl.java
+++ springboot-proxy-aop/src/main/java/com/example/proxy/service/impl/BMIServiceImpl.java
@@ -6,7 +6,8 @@ import com.example.proxy.service.BMIService;
 
 @Service
 public class BMIServiceImpl implements BMIService {
-
+	
+	// 單純計算 BMI 值
 	@Override
 	public Double getBMI(Double h, Double w) {
 		Double bmi = w / Math.pow(h/100, 2);
