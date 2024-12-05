e118cf85f7c9b3a6e5358bc867f1364ff2fc2faf
NTPU <NTPU@503-tr>
2024-12-05 09:54:00 +0800

    web

--- springboot-websocket-backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
+++ springboot-websocket-backend/src/main/java/com/example/websocket/config/WebSocketConfig.java
@@ -1,5 +1,21 @@
 package com.example.websocket.config;
 
-public class WebSocketConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
+import com.example.websocket.channel.ChatChannel;
+
+@Configuration
+public class WebSocketConfig {
+	
+	@Bean
+	public ServerEndpointExporter serverEndpointExporter() {
+		ServerEndpointExporter exporter = new ServerEndpointExporter();
+		// 註冊 WebSocket 端點
+		exporter.setAnnotatedEndpointClasses(ChatChannel.class);
+		
+		return exporter;
+	}
+	
 }
