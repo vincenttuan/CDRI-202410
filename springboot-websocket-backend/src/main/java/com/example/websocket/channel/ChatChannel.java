2ee986705c61a8f75b40e45e7bc6bfbf60d8d93b
NTPU <NTPU@503-tr>
2024-12-05 10:29:06 +0800

    web

--- springboot-websocket-backend/src/main/java/com/example/websocket/channel/ChatChannel.java
+++ springboot-websocket-backend/src/main/java/com/example/websocket/channel/ChatChannel.java
@@ -1,6 +1,10 @@
 package com.example.websocket.channel;
 
 import java.io.IOException;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import jakarta.websocket.CloseReason;
 import jakarta.websocket.OnClose;
@@ -18,24 +22,42 @@ import jakarta.websocket.server.ServerEndpoint;
 @ServerEndpoint(value = "/channel/chat")
 public class ChatChannel {
 	
+	// 利用 Set + synshronized 來保存所有 session
+	private static final Set<Session> sessions = Collections.synchronizedSet(new HashSet<Session>());
+	
+	// 廣播消息給所有連接用戶
+	private void broadcast(String sessionId, String message) {
+		sessions.forEach(session -> {
+					if(session.isOpen()) {
+						session.getAsyncRemote().sendText("[ " + sessionId + " 說]: " + message);
+					}
+				});
+	}
+	
 	// Session 指的是 WebSocket 的連線
 	// 每一個連線都有獨立的 session 與 id (自動分配)
 	@OnOpen // 當客戶端與伺服器建立連接時觸發。
 	public void onOpen(Session session) {
 		System.out.println("[ "+ session.getId() + " 已連線]");
-		// 回應
-		session.getAsyncRemote().sendText("[ "+ session.getId() + " 已連線]");
+		// 回應 (只有自己知道)
+		//session.getAsyncRemote().sendText("[ "+ session.getId() + " 已連線]");
+		// 廣播消息
+		broadcast(session.getId(), "已連線");
 	}
 	
 	@OnMessage // 當伺服器收到來自客戶端的消息時觸發。
 	public void onMessage(String message, Session session) {
-		// 回應
-		session.getAsyncRemote().sendText("[ " + session.getId() + " 說]: " + message);
+		// 回應 (只有自己知道)
+		//session.getAsyncRemote().sendText("[ " + session.getId() + " 說]: " + message);
+		// 廣播消息
+		broadcast(session.getId(), message);
 	}
 	
 	@OnClose // 當客戶端與伺服器的連接被關閉時觸發。
 	public void onClose(Session session, CloseReason closeReason) {
 		System.out.println("[ "+ session.getId() + " 已離線], 原因: " + closeReason);
+		// 廣播消息
+		broadcast(session.getId(), "離線了");
 	}
 	
 	@OnError // 當 WebSocket 通訊過程中發生錯誤時觸發。
