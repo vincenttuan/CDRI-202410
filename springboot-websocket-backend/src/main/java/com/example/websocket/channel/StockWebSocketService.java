ad2b8a62df0ebe13855ac8ca27d9c656ac4cdd89
NTPU <NTPU@503-tr>
2024-12-06 11:02:31 +0800

    web

---
+++ springboot-websocket-backend/src/main/java/com/example/websocket/channel/StockWebSocketService.java
@@ -0,0 +1,90 @@
+package com.example.websocket.channel;
+
+import java.time.LocalDateTime;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
+import jakarta.websocket.server.ServerEndpoint;
+
+@ServerEndpoint(value = "/stock-service")
+public class StockWebSocketService {
+	
+	// 訂閱時間服務的客戶Session集合 (那些 Session 有訂閱此服務)
+	private static final Set<Session> subscribers = Collections.synchronizedSet(new HashSet<>());
+	
+	// 定期任務執行器
+	private static final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	
+	static {
+		// 任務
+		Runnable task = () -> {
+			// 取得現在時間
+			String timeMessage = "現在時間: " + LocalDateTime.now();
+			// 發送通知給有訂閱的人
+			for(Session session : subscribers) {
+				sendMessage(session, timeMessage);
+			}
+		};
+		// 定期執行
+		scheduler.scheduleAtFixedRate(task, 0, 1, TimeUnit.SECONDS);
+		
+	}
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		// 發送歡迎消息
+		sendMessage(session, "Welcome! 可用指令: SUBSCRIBE_STOCK, UNSUBSCRIBE_STOCK");
+	}
+	
+	@OnMessage
+	public void onMessage(Session session, String message) {
+		switch(message) {
+			case "SUBSCRIBE_STOCK":
+				subscribers.add(session); // 加入訂閱
+				sendMessage(session, "[已訂閱]時間股票報價服務成功");
+				break;
+			case "UNSUBSCRIBE_STOCK":
+				subscribers.remove(session); // 取消訂閱
+				sendMessage(session, "[已取消]訂閱股票報價服務成功");
+				break;
+			default:
+				sendMessage(session, "指令錯誤! 可用指令: SUBSCRIBE_STOCK, UNSUBSCRIBE_STOCK");
+		}
+	}
+	
+	@OnClose
+	public void onClose(Session session) {
+		// 客戶端斷開連線時要取消該客戶的訂閱
+		subscribers.remove(session);
+		sendMessage(session, "[已取消]訂閱股票報價服務成功, 因客戶端斷開連線");
+	}
+	
+	@OnError
+	public void onError(Session session, Throwable throwable) {
+		// 出現錯誤時取消訂閱
+		subscribers.remove(session);
+		// 印出錯誤訊息
+		throwable.printStackTrace();
+	}
+	
+	// 發送訊息
+	private static void sendMessage(Session session, String message) {
+		try {
+			if(session.isOpen()) {
+				session.getBasicRemote().sendText(message); // 同步送出
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+}
---
+++ springboot-websocket-backend/src/main/java/com/example/websocket/util/YahooStockScraper.java
@@ -0,0 +1,33 @@
+package com.example.websocket.util;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class YahooStockScraper {
+	
+	public static Map<String, String> getPrice(String symbol) throws IOException {
+		String url = "https://tw.stock.yahoo.com/quote/" + symbol;
+		Document doc = Jsoup.connect(url).get();
+		// 找到 <ul> 下的 <li class="price-detail-item"> 前面幾個字是 price-detail-item 的元素
+		Elements elements = doc.select("ul > li.price-detail-item");
+		// 過濾 html tag
+		Map<String, String> priceMap = new HashMap<>();
+		elements.forEach(e -> {
+			// e 裡面有 2 個 <span> 分別印出 text
+			Elements spans = e.select("span");
+			String key = spans.get(0).text().replace(" ", "").replace("\n", "");
+			String value = spans.get(1).text().replace("%", "").replace(",", "").replace(" ", "").replace("\n", "");
+			priceMap.put(key, value);
+		});
+		return priceMap;
+	}
+}
+
+
+
