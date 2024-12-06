244116d14c836b053460bead191d3994b439ea8e
NTPU <NTPU@503-tr>
2024-12-06 09:49:21 +0800

    web

--- springboot-websocket-backend/src/main/java/com/example/websocket/channel/TimeWebSocketService.java
+++ springboot-websocket-backend/src/main/java/com/example/websocket/channel/TimeWebSocketService.java
@@ -1,8 +1,33 @@
 package com.example.websocket.channel;
 
+import jakarta.websocket.OnClose;
+import jakarta.websocket.OnError;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
 @ServerEndpoint(value = "/time-service")
 public class TimeWebSocketService {
+	
+	@OnOpen
+	public void onOpen(Session session) {
+		
+	}
+	
+	@OnMessage
+	public void onMessage(String message, Session session) {
+		
+	}
+	
+	@OnClose
+	public void onClose(Session session, Throwable throwable) {
+		
+	}
+	
+	@OnError
+	public void onOpen(Session session, String message) {
+		
+	}
 	
 }
