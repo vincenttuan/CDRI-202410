e86d43770f188e477328971af70ce3a83546872f
NTPU <NTPU@host.docker.internal>
2024-12-11 11:33:58 +0800

    web

--- JavaSecurity/src/hash/SalaryHashValidator.java
+++ JavaSecurity/src/hash/SalaryHashValidator.java
@@ -10,10 +10,13 @@ public class SalaryHashValidator {
 		String filePath = "src/hash/my_salary.txt";
 		String content = Files.readString(Path.of(filePath));
 		System.out.println(content);
-		String salaryHash = "4d3910e5b62e48b26033dac3af0dbe9f09f8f4e985cbf07c9f72dfb1412b70ee";
-		
+		String hashFilePath = "src/hash/my_salary_hash.txt";
+		String salaryHash = Files.readString(Path.of(hashFilePath));
+		System.out.println("原始:" + salaryHash);
+		System.out.println("----------------------------------------");
 		// 重新針對 filePath 計算 hash
 		String newSalaryHash = KeyUtil.generateFileHash(filePath);
+		System.out.println("新的:" + newSalaryHash);
 		// 比較 salaryHash.equals(newSalaryHash);
 		if(salaryHash.equals(newSalaryHash)) {
 			System.out.println("資料正確");
