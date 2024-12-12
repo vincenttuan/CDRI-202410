1e9a56875cc3757ff8bc8c370e320975b79c06f7
NTPU <NTPU@host.docker.internal>
2024-12-12 10:00:12 +0800

    web

--- JavaSecurity/src/mac/MACExample.java
+++ JavaSecurity/src/mac/MACExample.java
@@ -1,5 +1,7 @@
 package mac;
 
+import java.util.Arrays;
+
 import javax.crypto.SecretKey;
 
 import security.KeyUtil;
@@ -18,7 +20,22 @@ public class MACExample {
 		byte[] macValue = KeyUtil.generateMac(macKey, message); // 預設 HmacSHA256
 		
 		// 4. 將 macValue 轉 16 進位字串印出
-		System.out.println("MAC(Hex):" + KeyUtil.bytesToHex(macValue));
+		String macHexValue = KeyUtil.bytesToHex(macValue);
+		System.out.println("MAC(Hex):" + macHexValue);
+		
+		// 5. 在實際應用中, 接收方會收到訊息(message) 與 macHexValue
+		//    此時 message 要與 macKey(雙方都要有的密鑰) 產生出 computedMacHexValue 值進行與 macHexValue 的比對
+		String receivedMessage = message; // 收到的訊息
+		byte[] computedMacValue = KeyUtil.generateMac(macKey, receivedMessage);
+		String computedMacHexValue = KeyUtil.bytesToHex(computedMacValue);
+		
+		// 6. 比較 macHexValue 值是否相同 ?
+		if(macHexValue.equals(computedMacHexValue)) {
+			System.out.println("MAC 驗證成功, 來源正確");
+		} else {
+			System.out.println("MAC 驗證失敗, 來源有誤");
+		}
+		
 	}
 	
 }
