223783f5181182350ced94fccb365d287bdf76d7
NTPU <NTPU@host.docker.internal>
2024-12-12 10:13:36 +0800

    web

--- JavaSecurity/src/mac/SalaryProtectionCreator.java
+++ JavaSecurity/src/mac/SalaryProtectionCreator.java
@@ -1,5 +1,11 @@
 package mac;
 
+import java.io.File;
+
+import javax.crypto.SecretKey;
+
+import security.KeyUtil;
+
 /*
  * 情境描述：
  * 在一家大型企業，HR部門每月都會發送電子薪資明細給員工。
@@ -9,5 +15,22 @@ package mac;
  * macKey檔案位置: src/mac/macKey.key
  * */
 public class SalaryProtectionCreator {
-
+	public static void main(String[] args) throws Exception {
+		String filePath = "src/mac/my_salary.txt";
+		String keyPath = "src/mac/mackey.key";
+		
+		// 生成 mac key file
+		SecretKey macKey = null;
+		if(new File(keyPath).exists()) {
+			macKey = KeyUtil.getSecretKeyFromFile("HmacSHA256", keyPath);
+		} else {
+			macKey = KeyUtil.generateKeyForHmac();
+			// 儲存
+			KeyUtil.saveSecretKeyToFile(macKey, keyPath);
+		}
+		
+		// 得到 macValue(Hex)
+		String macHexValue = KeyUtil.generateMac("HmacSHA256", macKey, filePath);
+		System.out.println("HR 發布的 macHexValue: " + macHexValue);
+	}
 }
