64ae76052655bdee6ad226313f1615077a912cf5
NTPU <NTPU@503-tr>
2024-12-17 10:03:39 +0800

    web

---
+++ springboot-ssl/src/main/java/com/example/demo/filter/SecurityHeaderFilter.java
@@ -0,0 +1,32 @@
+package com.example.demo.filter;
+
+import java.io.IOException;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpFilter;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class SecurityHeaderFilter extends HttpFilter {
+
+	@Override
+	protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		
+		// HSTS Header
+		response.setHeader("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+		
+		// X-Frame-Options 防止點擊劫持攻擊
+		response.setHeader("X-Frame-Options", "DENY");
+		
+		// X-Content-Type-Options 防止 MIME 類型攻擊
+		response.setHeader("X-Content-Type-Options", "nosniff");
+		
+		// X-XSS-Protection 啟用 XSS 防護(防止外部惡意的 script 程式攻擊)
+		response.setHeader("X-XSS-Protection", "1; mode=block");
+		
+		chain.doFilter(request, response);
+	}
+	
+}
