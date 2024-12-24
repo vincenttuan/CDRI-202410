55bbb6f1084ad6a072e41f0a483b03e67157e7ff
NTPU <NTPU@503-tr>
2024-12-24 12:10:22 +0800

    j

--- JavaThread2/src/sync/Account.java
+++ JavaThread2/src/sync/Account.java
@@ -8,15 +8,15 @@ public class Account {
 		this.balance = balance;
 	}
 
-	public void withdraw(int amount) {
+	public synchronized void withdraw(int amount) {
 		String tName = Thread.currentThread().getName();
 		System.out.println(tName + " 進來提款 $" + amount);
 		int currentBalance = balance; // 取得最新餘額
-		try {
-			Thread.sleep(1); // 模擬延遲
-		} catch (Exception e) {
-			
-		}
+//		try {
+//			Thread.sleep(1000); // 模擬延遲
+//		} catch (Exception e) {
+//			
+//		}
 		if(currentBalance >= amount) {
 			balance = currentBalance - amount; // 修改餘額
 			System.out.println(tName + " 提款 $" + amount + " 成功, 餘額 $" + balance);
