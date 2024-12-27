d4f4d32a78acab4bb81f886bb75a38f0177b3095
NTPU <NTPU@503-tr>
2024-12-27 16:04:10 +0800

    web

--- springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
+++ springboot-jpa/src/main/java/com/example/leave/controller/EmployeeController.java
@@ -33,6 +33,7 @@ public class EmployeeController {
 	@Autowired
 	private LeaveRequestService leaveRequestService;
 	
+	// 取得員工資料
 	@GetMapping
 	public String index(Model model) {
 		List<EmployeeDTO> employees = employeeService.findAllEmployeeDTOs();
@@ -40,6 +41,7 @@ public class EmployeeController {
 		return "leave/employee";
 	}
 	
+	// 取得員工專案
 	@GetMapping("/{employeeId}/project")
 	public String employeeProject(@PathVariable Integer employeeId, Model model) {
 	    EmployeeDTO employeeDTO = employeeService.getEmployeeDTOById(employeeId);
@@ -51,13 +53,8 @@ public class EmployeeController {
 	    
 	    return "leave/employee_project";
 	}
-
-	@PostMapping("/{employeeId}/project")
-	public String employeeProjectUpdate(@PathVariable Integer employeeId, @RequestParam(required = false) List<Integer> projects) {
-	    employeeService.updateProject(employeeId, projects);
-	    return "redirect:/employee";
-	}
 	
+	// 取得員工假單(給新增假單用)
 	@GetMapping("/{employeeId}/leave_request")
 	public String employeeLeaveRequest(@PathVariable Integer employeeId, Model model) {
 	    EmployeeDTO employeeDTO = employeeService.getEmployeeDTOById(employeeId);
@@ -69,6 +66,7 @@ public class EmployeeController {
 	    return "leave/employee_leave_request";
 	}
 	
+	// 取得員工假單(給修改假單用)
 	@GetMapping("/{employeeId}/leave_request/{leaveRequestId}")
 	public String employeeLeaveRequestUpdate(@PathVariable Integer employeeId, @PathVariable Integer leaveRequestId, Model model) {
 	    EmployeeDTO employeeDTO = employeeService.getEmployeeDTOById(employeeId);
@@ -80,14 +78,21 @@ public class EmployeeController {
 	    return "leave/employee_leave_request";
 	}
 	
-	// 新增/修改假單
+	// 新增員工專案
+	@PostMapping("/{employeeId}/project")
+	public String employeeProjectUpdate(@PathVariable Integer employeeId, @RequestParam(required = false) List<Integer> projects) {
+	    employeeService.updateProject(employeeId, projects);
+	    return "redirect:/employee";
+	}
+		
+	// 新增/修改員工假單
 	@RequestMapping(value = "/{employeeId}/leave_request", method = {RequestMethod.POST, RequestMethod.PUT})
-	public String AddOrUpdateEmployeeLeaveRequest(@PathVariable Integer employeeId, @ModelAttribute LeaveRequestDTO leaveRequestDTO) {
+	public String addOrUpdateEmployeeLeaveRequest(@PathVariable Integer employeeId, @ModelAttribute LeaveRequestDTO leaveRequestDTO) {
 	    employeeService.addOrUpdateLeaveRequest(employeeId, leaveRequestDTO);
 		return "redirect:/employee";
 	}
 	
-	
+	// 刪除員工假單
 	@GetMapping("/{employeeId}/leave_request/delete/{leaveRequestId}")
 	public String index(@PathVariable Integer employeeId, @PathVariable Integer leaveRequestId) {
 		leaveRequestService.deleteLeaveRequest(leaveRequestId);
--- springboot-jpa/src/main/java/com/example/leave/repository/LeaveRequestRepository.java
+++ springboot-jpa/src/main/java/com/example/leave/repository/LeaveRequestRepository.java
@@ -1,5 +1,7 @@
 package com.example.leave.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +10,5 @@ import com.example.leave.model.entity.LeaveRequest;
 
 @Repository
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Integer> {
-
+	List<LeaveRequest> findByEmployeeId(Integer employeeId);
 }
